d2458cafe10ffae13268beb7ed04e3bf368efd5f
ttquyen <thuyquyentruong543@gmail.com>
2019-11-09 15:32:27 +0700

    Lam xong 3 bai

--- 18125110_W04/Library/mainLibrary.cpp
+++ 18125110_W04/Library/mainLibrary.cpp
@@ -1,10 +1,12 @@
 #include"DocGia.h"
 
 int main() {
+	cout << "----DOC GIA THUONG----\n";
 	DocGiaThuong a;
 	a.inputThuong();
 	a.displayThuong();
 	DocGiaVIP b;
+	cout << "----DOC GIA VIP----\n";
 	b.input();
 	b.displayVIP();
 	system("pause");
